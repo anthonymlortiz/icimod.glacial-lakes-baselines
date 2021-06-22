371b6082e690f077b6feefd723a49b02fc632f19
Kris Sankaran <sankaran.kris@gmail.com>
2021-06-22 14:58:57 -0500

    Add a validator

--- train.py
+++ train.py
@@ -38,32 +38,61 @@ def LSE_loss(phi_T, gts, sdts, epsilon=-1, eta=100):
     return eta * balanced_bce(lse.Heaviside(phi_T, epsilon=epsilon), gts)
 
 
+def validate(loader, net, T, dt_max, epsilon):
+    net.eval()
+    loss = 0
+    for x, y, dt, sdt in loader:
+        with torch.no_grad():
+            phi_0, energy, g = [interpolater(z, sdt.shape[-1]) for z in net(x)]
+            g = torch.sigmoid(g)
+            shift = 10 * np.random.rand() - 5
+            phi_T = lse.levelset_evolution(phi_0 + shift, energy, g, T, dt_max)
+            loss += LSE_loss(phi_T, y, sdt, epsilon).item()
+
+    return loss
+
+
 def interpolater(x, s):
     return F.interpolate(x, size=s, mode="bilinear", align_corners=True)
 
 
-def train(loaders, net, optimizer, epochs=20, T=4, epsilon=-1, dt_max=30):
-    for _ in range(epochs):
-        totals = 0
-        for x, y, dt, sdt in loaders["train"]:
-            optimizer.zero_grad()
-            phi_0, energy, g = net(x)
-            phi_0 = interpolater(phi_0, sdt.shape[-1])
-            energy = interpolater(energy, sdt.shape[-1])
-            g = torch.sigmoid(interpolater(g, sdt.shape[-1]))
-            shift = 10 * np.random.rand() - 5
-            phi_T = lse.levelset_evolution(sdt + shift, energy, g, T, dt_max)
+def train(loaders, net, optimizer, T=4, epsilon=-1, dt_max=30, epochs=40,
+          pretrain_epochs=20):
+    metrics = {"val": {"loss": []}, "train": {"loss": []}}
 
+    for epoch in range(epochs):
+        train_loss = 0
+        for x, y, dt, sdt in loaders["train"]:
+            net.train()
+
+            # make a forward pass on the new batch
+            optimizer.zero_grad()
+            phi_0, energy, g = [interpolater(z, sdt.shape[-1]) for z in net(x)]
+            g = torch.sigmoid(g)
+            shift = 10 * np.random.rand() - 5
             vfs = lse.gradient(y, split=False)
-            loss = mean_square_loss(phi_0, sdt) +\
-                vector_field_loss(energy, vfs) +\
-                LSE_loss(phi_T, y, sdt, epsilon)
+
+            # either pretrain or end-to-end train the network
+            if epoch < pretrain_epochs:
+                phi_T = lse.levelset_evolution(sdt + shift, energy, g, T, dt_max)
+                loss = mean_square_loss(phi_0, sdt) +\
+                    vector_field_loss(energy, vfs) +\
+                    LSE_loss(phi_T, y, sdt, epsilon)
+            else:
+                phi_T = lse.levelset_evolution(phi_0 + shift, energy, g, T, dt_max)
+                loss = LSE_loss(phi_T, y, sdt, epsilon)
+
+            # take a gradient step
             loss.backward()
             clip_grad_norm_(net.parameters(), 10)
             optimizer.step()
-            totals += loss.detach().item()
-        print(totals)
+            train_loss += loss.detach().item()
+        print(train_loss)
 
     # compute validation error
+    metrics["train"]["loss"].append(train_loss)
+    val_loss = validate(net, loaders["val"], T, dt_max, epsilon)
+    metrics["val"]["loss"].append(val_loss)
+
     # save logging information
     return net, optimizer

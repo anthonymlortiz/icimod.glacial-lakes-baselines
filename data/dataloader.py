3545d81fe0d0583d38855a97a0c127925db87520
Kris Sankaran <sankaran.kris@gmail.com>
2021-07-08 17:01:43 +0000

    specify label_channels in val loader

--- data/dataloader.py
+++ data/dataloader.py
@@ -51,20 +51,16 @@ def image_transforms(img): #, stats_fn, id):
     #mean, std = get_imagery_statistics(stats_fn, id)
     #img = (img - mean) / std
     img = np.rollaxis(img, 2, 0).astype(np.float32)[:3]
+    img = np.nan_to_num(img)
     img = torch.from_numpy(img)
     return img
 
 
 def get_imagery_statistics(stats_fn,id):
     df = pd.read_csv(stats_fn, index_col=0)
-    means = np.array([df["mean_0"][id], df["mean_1"][id], df["mean_2"][id], df["mean_3"][id], df["mean_4"][id],
-                     df["mean_5"][id], df["mean_6"][id], df["mean_7"][id], df["mean_8"][id], df["mean_9"][id],
-                     df["mean_10"][id]])
-    stds = np.array([df["sdev_0"][id], df["sdev_1"][id], df["sdev_2"][id], df["sdev_3"][id], df["sdev_4"][id],
-                    df["sdev_5"][id], df["sdev_6"][id], df["sdev_7"][id], df["sdev_8"][id], df["sdev_9"][id],
-                    df["sdev_10"][id]])
+    means = df.loc[id].filter(like="mean").values
+    stds = df.loc[id].filter(like="sdev").values
     return means, stds
-
 
 
 def load_dataset(opts, kwargs=None):
@@ -81,7 +77,7 @@ def load_dataset(opts, kwargs=None):
     val_label_fns = get_labels_fns(opts.data_dir , "val", opts.dataset)
     val_img_fns.sort()
     val_label_fns.sort()
-    val = StreamingGeospatialDataset(val_img_fns, stats_fn, val_label_fns, train_img_fns, chip_size=opts.chip_size, num_chips_per_tile=10, image_transform=img_transforms, verbose=False)
+    val = StreamingGeospatialDataset(val_img_fns, stats_fn, val_label_fns, opts.label_channels, chip_size=opts.chip_size, num_chips_per_tile=10, image_transform=img_transforms, verbose=False)
 
     trn_loader = torch.utils.data.DataLoader(trn, batch_size=opts.batch_size, num_workers=opts.num_workers, pin_memory=True)
     val_loader = torch.utils.data.DataLoader(val, batch_size=opts.batch_size, num_workers=opts.num_workers, pin_memory=True)

fixing normalization transforms when using groups

data/dataloader.py
@@ -51,16 +51,16 @@ def get_image_transforms():
     return image_transforms
 
 
-def image_transforms(img): #, stats_fn, id):
-    #mean, std = get_imagery_statistics(stats_fn, id)
-    #img = (img - mean) / std
-    img = np.rollaxis(img, 2, 0).astype(np.float32)[:3]
+def image_transforms(img, stats_fn, id): #, stats_fn, id):
+    mean, std = get_imagery_statistics(stats_fn, id)
+    img = (img - mean) / std
+    img = np.rollaxis(img, 2, 0).astype(np.float32)
     img = np.nan_to_num(img)
     img = torch.from_numpy(img)
     return img
 
 
-def get_imagery_statistics(stats_fn,id):
+def get_imagery_statistics(stats_fn, id):
     df = pd.read_csv(stats_fn, index_col=0)
     means = df.loc[id].filter(like="mean").values
     stds = df.loc[id].filter(like="sdev").values
@@ -77,15 +77,14 @@ def load_dataset(opts, kwargs=None):
     train_label_fns.sort()
     train_meta_fns.sort()
     stats_fn = get_stats_fn(opts.data_dir, "train", opts.dataset)
-    trn = StreamingGeospatialDataset(train_img_fns, stats_fn, train_label_fns, train_meta_fns, chip_size=opts.chip_size, num_chips_per_tile=20, image_transform=img_transforms, verbose=False)
+    trn = StreamingGeospatialDataset(train_img_fns, stats_fn, train_label_fns,train_meta_fns,  groups=train_img_fns, label_channels=opts.label_channels, chip_size=opts.chip_size, num_chips_per_tile=20, image_transform=img_transforms, verbose=False)
 
     val_img_fns = get_imagery_fns(opts.data_dir, "val", opts.dataset)
     val_label_fns = get_labels_fns(opts.data_dir , "val", opts.dataset)
     val_meta_fns = get_meta_fns(opts.data_dir , "val", opts.dataset)
     val_img_fns.sort()
     val_label_fns.sort()
-    val_meta_fns.sort()
-    val = StreamingGeospatialDataset(val_img_fns, stats_fn, val_label_fns, val_meta_fns, chip_size=opts.chip_size, num_chips_per_tile=10, image_transform=img_transforms, verbose=False)
+    val = StreamingGeospatialDataset(val_img_fns, stats_fn, val_label_fns, val_meta_fns, groups=val_img_fns, chip_size=opts.chip_size, num_chips_per_tile=10, image_transform=img_transforms, verbose=False)
 
     trn_loader = torch.utils.data.DataLoader(trn, batch_size=opts.batch_size, num_workers=opts.num_workers, pin_memory=True)
     val_loader = torch.utils.data.DataLoader(val, batch_size=opts.batch_size, num_workers=opts.num_workers, pin_memory=True)

data/streaming_dataset.py
@@ -154,7 +154,7 @@ class StreamingGeospatialDataset(IterableDataset):
                         if self.groups is None:
                             img = self.image_transform(img)
                         else:
-                            img = self.image_transform(img, self.stats_fn, group)
+                            img = self.image_transform(img, self.stats_fn, img_fn)
                     else:
                         img = torch.from_numpy(img).squeeze()
 
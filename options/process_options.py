fc4d3693da100eb2e85c1778e44d365967fe6e21
Kris Sankaran <sankaran.kris@gmail.com>
2021-07-28 17:27:19 +0000

    separate parser for preprocessing

---
+++ options/process_options.py
@@ -0,0 +1,39 @@
+import argparse
+
+class ProcessOptions():
+    def __init__(self):
+        self.initialized = False
+
+    def initialize(self, parser):
+        parser.add_argument('--in_dir', type=str, default='/datadrive/snake/lakes/le7-2015/', help='Absolute path to imagery to preprocess')
+        parser.add_argument('--label_path', type=str, default='/datadrive/snake/lakes/GL_3basins_2015.shp', help='Absolute path to shapefiles to use for labeling')
+        parser.add_argument('--out_dir', type=str, default='/datadrive/snake/lakes/le7-2015/test/', help='Absolute path to directory to save train / val / test splits')
+        self.initialized = True
+        return parser
+
+    def print_options(self, opt):
+        message = ''
+        message += '----------------- Options ---------------\n'
+        for k, v in sorted(vars(opt).items()):
+            comment = ''
+            default = self.parser.get_default(k)
+            if v != default:
+                comment = '\t[default: %s]' % str(default)
+            message += '{:>25}: {:<30}{}\n'.format(str(k), str(v), comment)
+        message += '----------------- End -------------------'
+        print(message)
+
+    def gather_options(self):
+        # initialize parser with basic options
+        if not self.initialized:
+            parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+            parser = self.initialize(parser)
+
+        opt, _ = parser.parse_known_args()
+        self.parser = parser
+        return opt
+
+    def parse(self, save=False):
+        opt = self.gather_options()
+        self.print_options(opt)
+        return opt

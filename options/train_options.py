e9dcd4c88f3cf1b53f0c9a57c9718cc47e108976
Kris Sankaran <sankaran.kris@gmail.com>
2021-08-16 14:04:38 +0000

    update processing options

--- options/train_options.py
+++ options/train_options.py
@@ -6,7 +6,7 @@ class TrainOptions(BaseOptions):
         BaseOptions.initialize(self, parser)
 
         # Options for training
-        parser.add_argument('--n_epochs', type=int, default=10, help='# of training epochs')
+        parser.add_argument('--n_epochs', type=int, default=30, help='# of training epochs')
         parser.add_argument('--optimizer', type=str, default='adam', help='optimizer for training model [adam | sgd]')
         parser.add_argument('--beta1', type=float, default=0.9, help='momentum term of adam')
         parser.add_argument('--beta2', type=float, default=0.999, help='momentum term of adam')
@@ -16,7 +16,7 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--loss', type=str, default='wbce', help='loss for training model [ce | wbce | delse]')
         parser.add_argument('--scheduler_patience', type=int, default=5, help='lr scheduler patience')
 
-        parser.add_argument('--max_grad_norm', type=float, default=100, help='Gradient clipping')
+        parser.add_argument('--max_grad_norm', type=float, default=10, help='Gradient clipping')
         parser.add_argument('--val_metric', type=str, default='IoU', help='Which metric to use for model selection? [IoU | precision | recall]')
         parser.add_argument('--save_epoch', type=int, default=10, help='How frequently to save checkpoints?')
         self.isTrain = True

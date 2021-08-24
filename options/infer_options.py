67228d99a1389bf091c120a3d7a4fe98e155554d
Kris Sankaran <sankaran.kris@gmail.com>
2021-08-24 16:22:37 +0000

    storing boolean for processing and inference

--- options/infer_options.py
+++ options/infer_options.py
@@ -9,7 +9,7 @@ class InferOptions(BaseOptions):
         parser.add_argument('--meta_dir', type=str, default='sentinel/splits/train/meta', help='Relative paths to metadata on which to perform inference')
         parser.add_argument('--inference_dir', type=str, default='/datadrive/results/inference/sentinel_train-unet/', help='Absolute path to directory where results should be saved')
         parser.add_argument('--stats_fn', type=str, default='sentinel/splits/train/statistics.csv', help='Relative path to stats.csv')
-        parser.add_argument('--divergence', type=bool, default=False, action='store_true', help='Add divergence feature?')
+        parser.add_argument('--divergence', default=False, action='store_true', help='Add divergence feature?')
         parser.add_argument('--model_pth', type=str, default='/datadrive/results/backup/sentinel-unet_best.pth', help='Absolute path to model')
         parser.add_argument('--chip_size', type=int, default=256, help='Model input / output chip dimension')
         return parser

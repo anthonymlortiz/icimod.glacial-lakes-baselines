2ce681eee696105eed0a0d3bcc248842d7e68fcd
Anthony Ortiz <anthony.ortiz@microsoft.com>
2021-10-18 18:10:36 +0000

    Moving snake functions to utils, adding snake post-processing for UNet

---
+++ utils/snake_utils.py
@@ -0,0 +1,89 @@
+import os
+import fiona
+from shapely.geometry import Polygon
+from functools import partial
+from shapely.ops import transform
+
+import pyproj
+import warnings
+
+import rasterio
+from rasterio.features import shapes
+from shapely.geometry import shape
+
+from utils import data
+
+
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
+def buffer_polygon_in_meters(polygon, buffer, percentage=0.25):
+    proj_meters = pyproj.Proj('epsg:3857')
+    proj_latlng = pyproj.Proj('epsg:4326')
+
+    project_to_meters = partial(pyproj.transform, proj_latlng, proj_meters)
+    project_to_latlng = partial(pyproj.transform, proj_meters, proj_latlng)
+
+    pt_meters = transform(project_to_meters, polygon)
+    buffer_meters = pt_meters
+
+    while buffer_meters.area > percentage * pt_meters.area:
+        buffer_meters = buffer_meters.buffer(buffer)
+    buffer_polygon = transform(project_to_latlng, buffer_meters.buffer(0))
+
+    return buffer_polygon
+
+
+def get_bing_glid_from_fn(bing_fn):
+    base = os.path.basename(bing_fn)
+    gl_id = os.path.splitext(base)[0]
+    return gl_id
+
+
+def get_sentinel_glid_from_fn(bing_fn):
+    base = os.path.basename(bing_fn)
+    gl_id, _ = base.split("_")
+    return gl_id
+
+
+def get_glacial_lake_2015_outline_from_glid(lakes_shapefile_2015, glid):
+    with fiona.open(lakes_shapefile_2015, "r") as shape_file:
+        for feature in shape_file:
+            if glid == feature['properties']["GL_ID"]:
+                area = feature['properties']["Area"]
+                polygon = Polygon(feature["geometry"]["coordinates"][0])
+                return area, polygon
+
+
+def polygonize_raster_and_buffer(raster_fn, border_pixels=20):
+    """Open and polygonize raster file."""
+    mask = None
+    with rasterio.Env():
+        with rasterio.open(raster_fn) as src:
+            image = src.read(1)[border_pixels:-border_pixels, border_pixels:-border_pixels] 
+            results = (
+                        {'properties': {'raster_val': v}, 'geometry': s}
+                        for i, (s, v) 
+                        in enumerate(
+                        shapes(image, mask=mask, transform=src.transform)))
+    geometries = list(results)
+    geoms = [geom for geom in geometries if geom['properties']['raster_val'] == 1]
+    xy_polygons = []
+    for geom in geoms:
+        poly = shape(geom['geometry'])
+        xy_buffered_polygon = []
+        proj_meters = pyproj.Proj('epsg:3857')
+        proj_latlng = pyproj.Proj('epsg:4326')
+
+        project_to_meters = partial(pyproj.transform, proj_latlng, proj_meters)
+        pt_meters = transform(project_to_meters, poly)
+        area = pt_meters.area/1000000
+
+        buffer_size = data.get_buffer_from_area(area)
+        buffered_polygon = buffer_polygon_in_meters(poly, buffer_size, percentage=.6)
+        for i, (lon, lat) in enumerate(buffered_polygon.exterior.coords):
+            py, px = src.index(lon, lat)
+            xy_buffered_polygon.append((px, py))
+        xy_polygons.append(xy_buffered_polygon)
+    return xy_polygons
